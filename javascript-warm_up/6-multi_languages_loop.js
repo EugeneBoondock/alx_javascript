fea59ba423b403ee932c8d1d53fbc45bd369a700
EugeneBoondock <philosncube@gmail.com>
2023-09-06 16:02:35 +0000

    Fix

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+function printStrings(strings) {
+  for (let i = 0; i < strings.length; i++) {
+    console.log(strings[i].trim());
+  }
+}
+
+const stringsArray = ["C is fun\n", "Python is cool\n", "JavaScript is amazing"];
+printStrings(stringsArray);
