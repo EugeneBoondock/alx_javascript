b7b56967ac964534cccb981185cd307d102265c5
Eugene Ncube <philosncube@gmail.com>
2023-09-13 17:57:43 +0200

    quickfix

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,13 @@
 //class for Rectangle
 class Rectangle {
     constructor(w, h) {
-        this.width = w;
-        this.height = h;
-        
         if (w <= 0 || h <= 0) {
             // If w or h is not a positive integer or is 0, create an empty object
-            return {};
+            this.width = null;
+            this.height = null;
+      }else {
+        this.width = w;
+        this.height = h;
       }
     }
   }
