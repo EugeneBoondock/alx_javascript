38fc4370dcca8f72ba0ebcfa285aef0dbe61ae49
Eugene Ncube <philosncube@gmail.com>
2023-09-14 00:39:11 +0200

    x draw

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+//class for Rectangle
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        Object.assign(this, {});
+      }
+    }
+  
+    // Define an instance method
+    instmethod() {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
+  
