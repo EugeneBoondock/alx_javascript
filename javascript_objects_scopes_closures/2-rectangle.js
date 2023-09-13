9ed3c6af8fa94a73a746cd5c7e16c2f0a791ec4f
Eugene Ncube <philosncube@gmail.com>
2023-09-13 17:11:30 +0200

    if js

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+//class for Rectangle
+class Rectangle {
+    // Script defines rectangle
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+
+        if(w == 0 || h == 0 ) {
+            
+        }
+    }
+    }
+    
+    module.exports = Rectangle;
