6ede44c0afb0dab4d45d6dc9fd7879169fdb4e37
Eugene Ncube <philosncube@gmail.com>
2023-09-13 17:47:30 +0200

    fix again

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,10 +4,12 @@ class Rectangle {
     constructor(w, h) {
         this.width = w;
         this.height = h;
+        this.isValid = true;
         
         if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        // If w or h is not a positive integer or is 0, create an empty object
-            return console.log('Rectangle {}') && {};
+            // If w or h is not a positive integer or is 0, create an empty object
+            this.isValid = false;
+            return {};
       }
     }
   }
