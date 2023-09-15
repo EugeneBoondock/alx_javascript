f06891abd3132b590886f081454962c6a1da50f8
Eugene Ncube <philosncube@gmail.com>
2023-09-15 12:46:23 +0200

    double

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+// Class for Rectangle
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        Object.assign(this, {});
+      }
+    }
+  
+    // Define an instance method called print()
+    print() {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+
+    rotate() {
+        if (this.width && this.height) {
+            for (let i = 0; i < this.width; i++) {
+                console.log('X'.repeat(this.height));
+            }
+        }
+    }
+
+    double() {
+        if (this.width && this.height) {
+            for (let i = 0; i < this.height * 2; i++) {
+                console.log('X'.repeat(this.width * 2))
+            }
+        }
+    }
+  }
+  
+  module.exports = Rectangle;
+  
