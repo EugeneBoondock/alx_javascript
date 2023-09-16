ac2fdd50f015eba4122f46a6a8ffe8ae635a9595
Eugene Ncube <philosncube@gmail.com>
2023-09-16 23:47:07 +0200

    fix

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
+
 const myObject = {
     type: 'object',
     value: 12,
-    incr: function() {
+    incr: function () {
       this.value++;
-    }
+    },
+    inspect: function () {
+      return `{ type: '${this.type}', value: ${this.value}${this.incr ? `, incr: [Function]` : ''} }`;
+    },
   };
+  
   console.log(myObject);
   
   myObject.incr();
