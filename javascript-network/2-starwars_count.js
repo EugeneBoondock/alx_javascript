b5825504d08f64a8c8584efd5539cf0f433cf8e3
Eugene Ncube <philosncube@gmail.com>
2023-09-16 22:33:36 +0200

    char

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+const id = 18;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Request failed with status code:', response.statusCode);
+  } else {
+    try {
+      const filmData = JSON.parse(body);
+      const moviesWithCharacter = filmData.results.filter((movie) =>
+        movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)
+      );
+      console.log(moviesWithCharacter.length);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
+  }
+});
