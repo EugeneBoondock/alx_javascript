acb8c56e8328078ff55697ee22ea9664ef243a54
Eugene Ncube <philosncube@gmail.com>
2023-09-16 21:53:32 +0200

    hotfix

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-//A GET request
-const request = require('request');
-const url = ''
 
-request.get(url, {encoding: 'utf-8'}) 
-    .on('error', err => {
-        console.log('code: ' + err)
-    })
+//Get request for earth2
+const request = require('request');
+const url = 'https://earth2.io'; 
+
+request.get(url, { encoding: 'utf-8' })
+  .on('response', response => {
+    console.log('code:', response.statusCode);
+  })
+  .on('error', err => {
+    console.error('Error:', err);
+  });
