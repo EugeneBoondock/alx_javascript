73b1c20bef2c9bf80d1c6f36efc8d3a1c935d300
Eugene Ncube <philosncube@gmail.com>
2023-09-16 22:20:14 +0200

    fix

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 
 const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
+const url = "https://swapi-api.alx-tools.com/api/films/${id}";
 
-request.get(url, { encoding: 'utf-8' })
-  .on('response', response => {
-      response.on('end', () => {
-        const film = JSON.parse(data);
-        console.log(film.title);
-    })});
+request.get(url, { encoding: "utf-8" })
+ .on("response", (response) => {
+  if (response.statusCode === 200) {
+    let data = "";
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    response.on("end", () => {
+      const film = JSON.parse(data);
+      console.log(film.title);
+    });
+  }
+});
