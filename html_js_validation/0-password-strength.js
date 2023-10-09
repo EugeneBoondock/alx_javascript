1afe4e2f882d4d4254ca558a861208f082345083
Eugene Ncube <philosncube@gmail.com>
2023-10-10 01:39:18 +0200

    password

--- html_js_validation/0-password-strength.js
+++ html_js_validation/0-password-strength.js
@@ -1,19 +1,25 @@
 function validatePassword () {
     var password = document.getElementById('password');
-    if (password.length == 0) {
+    var error = document.getElementById('error');
+
+    if (password.length < 8) {
         error.innerHTML = 'Password should be 8 characters long';
-        return false
+        return false;
     }
+
     if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/)){
         error.innerHTML = 'Password should contain at least 1 uppercase, lowercase, 1 special character and be 8 characters long';
     }
 
-    error.innerHTML.style.color = green 
+    error.innerHTML = '';
+    return true;
 }
 
 function validateForm () {
     var form = document.getElementById('passwordForm');
+    var error = document.getElementById('error');
+
     if (!validatePassword) {
-        
+        error.innerHTML = 'Please fix the errors'
     }
 }
