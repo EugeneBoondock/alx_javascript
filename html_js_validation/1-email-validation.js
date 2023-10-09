87909c80213e0be8b1949b7efaaec15ecc66ec50
Eugene Ncube <philosncube@gmail.com>
2023-10-10 01:50:59 +0200

    email

---
+++ html_js_validation/1-email-validation.js
@@ -0,0 +1,15 @@
+function validateEmail () {
+    var email = document.getElementById('email').value;
+    var error = document.getElementById('error');
+
+    if (email.length === 0) {
+        error.innerHTML = 'Please enter a valid email address.';
+        return false;
+    }
+    if (!email.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)) {
+        error.innerHTML = 'Please enter valid email address.';
+        return false;
+    }
+    error.innerHTML = '';
+    return true;
+}
