3cf8e058de940f06f4df464e7cea25f296b3b57c
Eugene Ncube <philosncube@gmail.com>
2023-10-21 23:47:45 +0200

    hotfix

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,25 +1,17 @@
-const req = require("request");
-let char = [];
-const id = 18;
-const url = `https://swapi-api.alx-tools.com/api/films`;
-req.get(url, (error, response, body) => {
-  if (error) {
-    console.error("Error:", error);
-    process.exit(1);
-  }
-  try {
-    const movies = JSON.parse(body);
-    const moviesres = movies.results;
-    moviesres.forEach((element) => {
-      element.characters.forEach((el) => {
-        if (el.includes(id)) {
-          char.push(el);
-        }
-      });
-    });
-    console.log(char.length);
-  } catch (parseError) {
-    console.error("Error parsing JSON:", parseError);
-    process.exit(1);
-  }
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const characterId = 18;
+
+request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const films = JSON.parse(body).results;
+        let count = 0;
+        films.forEach(film => {
+            if (film.characters.includes(characterId)) {
+                count++;
+            }
+        });
+        console.log(count);
+    }
 });
