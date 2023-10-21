6ce9fc0cf0fd10f264f61b647996b994c9766137
Eugene Ncube <philosncube@gmail.com>
2023-10-21 23:49:13 +0200

    fix

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,17 +1,28 @@
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-const characterId = 18;
+if (process.argv.length !== 3) {
+  console.error('Usage: node 2-starwars_count.js <API_URL>');
+  process.exit(1);
+}
 
-request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        const films = JSON.parse(body).results;
-        let count = 0;
-        films.forEach(film => {
-            if (film.characters.includes(characterId)) {
-                count++;
-            }
-        });
-        console.log(count);
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  const data = JSON.parse(body);
+  const films = data.results;
+
+  let count = 0;
+  for (let i = 0; i < films.length; i++) {
+    const characters = films[i].characters;
+    if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
     }
+  }
+
+  console.log(count);
 });
