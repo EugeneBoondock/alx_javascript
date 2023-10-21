d86b66d96586c8b803b6fff835c34328268a65c8
Eugene Ncube <philosncube@gmail.com>
2023-10-21 23:55:03 +0200

    hotfix

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,38 +1,34 @@
-#!/usr/bin/node
-
-function changeMode(size, weight, transform, background,  color) {
+function changeMode(size, weight, transform, background, color) {
   return function() {
-    document.body.style.font-size = size + 'px';
-    document.body.style.font-weight = weight;
-    document.body.style.text-transform = transform;
-    document.body.style.background-color = background;
-    document.body.style.color = color;
+      document.body.style.fontSize = size + 'px';
+      document.body.style.fontWeight = weight;
+      document.body.style.textTransform = transform;
+      document.body.style.backgroundColor = background;
+      document.body.style.color = color;
   };
 }
 
 function main() {
-  var spooky = changeMode(9, bold, uppercase, pink, green);
-  var darkMode = changeMode(12, bold, capitalize, black, white);
-  var screamMode = changeMode(12, normal, lowercase, white, black);
+  var spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  var darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+  var screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
 
   const paragraph = document.createElement('p');
   paragraph.textContent = 'Welcome Holberton!';
-
   document.body.appendChild(paragraph);
 
   const buttonsDiv = document.createElement('div');
 
-  const spookyButton = document.createElement('button')
+  const spookyButton = document.createElement('button');
   spookyButton.textContent = 'Spooky';
   spookyButton.addEventListener('click', spooky);
-  
 
   const darkModeButton = document.createElement('button');
-  darkModeButton.textContent = 'DarkMode';
+  darkModeButton.textContent = 'Dark Mode';
   darkModeButton.addEventListener('click', darkMode);
-  
-  const screamModeButton= document.createElement('button');
-  screamModeButton.textContent = 'ScreamMode';
+
+  const screamModeButton = document.createElement('button');
+  screamModeButton.textContent = 'Scream Mode';
   screamModeButton.addEventListener('click', screamMode);
 
   buttonsDiv.appendChild(spookyButton);
@@ -41,4 +37,5 @@ function main() {
 
   document.body.appendChild(buttonsDiv);
 }
+
 main();
